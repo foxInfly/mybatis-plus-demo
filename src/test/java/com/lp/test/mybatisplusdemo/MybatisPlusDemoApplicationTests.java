58b49ee431359c940c3748d4fb02f51460834dd6
lipu <lipu@zgzhongnan.com>
2020-12-12 14:28:52 +0800

    Initial commit

---
+++ src/test/java/com/lp/test/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.test.mybatisplusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
